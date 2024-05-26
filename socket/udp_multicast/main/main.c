ef865831546c882b6d4d801697b1889b6c161c18
chenwenhuiGithub <229306062@qq.com>
2024-05-26 15:19:59 +0800

    add socket udp_multicast example

---
+++ socket/udp_multicast/main/main.c
@@ -0,0 +1,173 @@
+#include "nvs_flash.h"
+#include "nvs.h"
+#include "esp_log.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include <lwip/netdb.h>
+
+#define EXAMPLE_WIFI_SSID               "TP-LINK_8E86"
+#define EXAMPLE_WIFI_PWD                "12345678"
+#define EXAMPLE_WIFI_MAX_RECONNECT      3
+#define EXAMPLE_UDP_MULTICAST_IP        "225.10.100.200"
+#define EXAMPLE_UDP_MULTICAST_PORT      60001
+
+static const char *TAG = "udp_multicast";
+static uint32_t wifi_reconnect_cnt = 0;
+
+static void udp_server_task(void *pvParameters)
+{
+    int sock = 0;
+    int err = 0;
+    int rx_len = 0;
+    uint8_t rx_buf[256] = { 0 };
+    struct sockaddr_in local_addr = { 0 };
+    struct sockaddr_in client_addr = { 0 };
+    socklen_t client_addr_len = sizeof(client_addr);
+    uint8_t ttl = 5;
+    uint8_t loopback = 0;
+    struct ip_mreq imreq = { 0 };
+
+    sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+    if (sock < 0) {
+        ESP_LOGE(TAG, "socket create failed:%d", errno);
+        goto exit;
+    }
+
+    local_addr.sin_family = AF_INET;
+    local_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    local_addr.sin_port = htons(EXAMPLE_UDP_MULTICAST_PORT);
+    err = bind(sock, (struct sockaddr *)&local_addr, sizeof(local_addr));
+    if (err != 0) {
+        ESP_LOGE(TAG, "socket bind failed:%d", errno);
+        goto exit;
+    }
+
+    err = setsockopt(sock, IPPROTO_IP, IP_MULTICAST_TTL, &ttl, sizeof(ttl));
+    if (err < 0) {
+        ESP_LOGE(TAG, "socket set IP_MULTICAST_TTL failed:%d", errno);
+        goto exit;
+    }
+
+    err = setsockopt(sock, IPPROTO_IP, IP_MULTICAST_LOOP, &loopback, sizeof(loopback));
+    if (err < 0) {
+        ESP_LOGE(TAG, "socket set IP_MULTICAST_LOOP failed:%d", errno);
+        goto exit;
+    }
+
+    imreq.imr_multiaddr.s_addr = inet_addr(EXAMPLE_UDP_MULTICAST_IP);
+    imreq.imr_interface.s_addr = htonl(INADDR_ANY);
+    err = setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, &imreq, sizeof(imreq));
+    if (err < 0) {
+        ESP_LOGE(TAG, "socket set IP_ADD_MEMBERSHIP failed:%d", errno);
+        goto exit;
+    }
+
+    ESP_LOGI(TAG, "udp multicast server listen, %s:%u", EXAMPLE_UDP_MULTICAST_IP, EXAMPLE_UDP_MULTICAST_PORT);
+    while (1) {
+        rx_len = recvfrom(sock, rx_buf, sizeof(rx_buf), 0, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (rx_len < 0) {
+            ESP_LOGE(TAG, "socket recv failed:%d", errno);
+            break;
+        } else {
+            ESP_LOGI(TAG, "recv %s:%d %s", inet_ntoa(client_addr.sin_addr), client_addr.sin_port, rx_buf);
+            sendto(sock, rx_buf, rx_len, 0, (struct sockaddr *)&client_addr, client_addr_len);
+        }
+    }
+
+exit:
+    vTaskDelete(NULL);
+}
+
+static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
+{
+    wifi_event_sta_connected_t* conn_event = NULL;
+    wifi_event_sta_disconnected_t* dis_event = NULL;
+    ip_event_got_ip_t* got_event = NULL;
+
+    if (WIFI_EVENT == event_base) {
+        switch (event_id) {
+        case WIFI_EVENT_STA_START:
+            ESP_LOGI(TAG, "wifi start connect, %s:%s", EXAMPLE_WIFI_SSID, EXAMPLE_WIFI_PWD);
+            esp_wifi_connect(); // non-block
+            break;
+        case WIFI_EVENT_STA_CONNECTED:
+            conn_event = (wifi_event_sta_connected_t *)event_data;
+            ESP_LOGI(TAG, "wifi connected, channel:%u authmode:%u", conn_event->channel, conn_event->authmode);
+            break;
+        case WIFI_EVENT_STA_DISCONNECTED:
+            dis_event = (wifi_event_sta_disconnected_t *)event_data;
+            ESP_LOGE(TAG, "wifi disconnected, reason:%u", dis_event->reason);
+            if (wifi_reconnect_cnt < EXAMPLE_WIFI_MAX_RECONNECT) {
+                wifi_reconnect_cnt++;
+                ESP_LOGW(TAG, "wifi start reconnect, cnt:%u", wifi_reconnect_cnt);
+                esp_wifi_connect();
+            } else {
+                ESP_LOGE(TAG, "wifi connect failed");
+            }
+            break;
+        default:
+            ESP_LOGW(TAG, "unknown WIFI_EVENT:%d", event_id);
+            break;
+        }
+    }
+    
+    if (IP_EVENT == event_base) {
+        switch (event_id) {
+        case IP_EVENT_STA_GOT_IP:
+            got_event = (ip_event_got_ip_t *)event_data;
+            ESP_LOGI(TAG, "got ip, ip:" IPSTR " netmask:" IPSTR " gw:" IPSTR,
+                IP2STR(&got_event->ip_info.ip), IP2STR(&got_event->ip_info.netmask), IP2STR(&got_event->ip_info.gw));
+            xTaskCreate(udp_server_task, "udp_server", 4096, NULL, 5, NULL);
+            break;
+        case IP_EVENT_STA_LOST_IP:
+            ESP_LOGE(TAG, "lost ip");
+            break;
+        default:
+            ESP_LOGW(TAG, "unknown IP_EVENT:%d", event_id);
+            break;   
+        }
+    }
+}
+
+void app_main(void)
+{
+    esp_err_t err = ESP_OK;
+    
+    err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        nvs_flash_erase();
+        err = nvs_flash_init();
+    }
+    if (ESP_OK != err) {
+        ESP_LOGE(TAG, "nvs_flash_init error:%d", err);
+        return;
+    }
+
+    esp_event_loop_create_default();
+    esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, NULL);
+    esp_event_handler_instance_register(IP_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, NULL);
+
+    esp_netif_init();
+    esp_netif_create_default_wifi_sta();
+    wifi_init_config_t init_cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&init_cfg);
+
+    wifi_config_t sta_config = {
+        .sta = {
+            .ssid = EXAMPLE_WIFI_SSID,
+            .password = EXAMPLE_WIFI_PWD,
+        },
+    };
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_set_config(WIFI_IF_STA, &sta_config);
+    esp_wifi_start(); // trigger WIFI_EVENT_STA_START
+
+    while (1) {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
