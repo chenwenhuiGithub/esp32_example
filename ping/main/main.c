15e5db1b62066a45e2167745807ec3fda405bc6d
chenwenhui <chenwenhui@solaxpower.com>
2024-09-20 16:43:04 +0800

    add ping example

---
+++ ping/main/main.c
@@ -0,0 +1,175 @@
+#include "nvs_flash.h"
+#include "nvs.h"
+#include "esp_log.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include "lwip/err.h"
+#include "lwip/netdb.h"
+#include "ping/ping_sock.h"
+
+
+#define EXAMPLE_WIFI_SSID                       "TP-LINK_wenhui"
+#define EXAMPLE_WIFI_PWD                        "12345678"
+#define EXAMPLE_PING_SERVER                     "www.baidu.com"
+
+static const char *TAG = "ping";
+static esp_ping_handle_t hd_ping = NULL;
+
+
+static void ping_success_cb(esp_ping_handle_t hdl, void *args)
+{
+    uint8_t ttl;
+    uint16_t seqno;
+    uint32_t elapsed_time, recv_len;
+    ip_addr_t target_addr;
+
+    esp_ping_get_profile(hdl, ESP_PING_PROF_SEQNO, &seqno, sizeof(seqno));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_TTL, &ttl, sizeof(ttl));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_IPADDR, &target_addr, sizeof(target_addr));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_SIZE, &recv_len, sizeof(recv_len));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_TIMEGAP, &elapsed_time, sizeof(elapsed_time));
+    printf("%" PRIu32 " bytes from %s icmp_seq=%" PRIu16 " ttl=%" PRIu16 " time=%" PRIu32 " ms\n",
+           recv_len, ipaddr_ntoa((ip_addr_t*)&target_addr), seqno, ttl, elapsed_time);
+}
+
+static void ping_timeout_cb(esp_ping_handle_t hdl, void *args)
+{
+    uint16_t seqno;
+    ip_addr_t target_addr;
+
+    esp_ping_get_profile(hdl, ESP_PING_PROF_SEQNO, &seqno, sizeof(seqno));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_IPADDR, &target_addr, sizeof(target_addr));
+    printf("From %s icmp_seq=%d timeout\n",ipaddr_ntoa((ip_addr_t*)&target_addr), seqno);
+}
+
+static void ping_end_cb(esp_ping_handle_t hdl, void *args)
+{
+    ip_addr_t target_addr;
+    uint32_t transmitted;
+    uint32_t received;
+    uint32_t total_time_ms;
+    uint32_t loss;
+
+    esp_ping_get_profile(hdl, ESP_PING_PROF_REQUEST, &transmitted, sizeof(transmitted));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_REPLY, &received, sizeof(received));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_IPADDR, &target_addr, sizeof(target_addr));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_DURATION, &total_time_ms, sizeof(total_time_ms));
+
+    if (transmitted > 0) {
+        loss = (uint32_t)((1 - ((float)received) / transmitted) * 100);
+    } else {
+        loss = 0;
+    }
+    if (IP_IS_V4(&target_addr)) {
+        printf("\n--- %s ping statistics ---\n", inet_ntoa(*ip_2_ip4(&target_addr)));
+    } else {
+        printf("\n--- %s ping statistics ---\n", inet6_ntoa(*ip_2_ip6(&target_addr)));
+    }
+    printf("%" PRIu32 " packets transmitted, %" PRIu32 " received, %" PRIu32 "%% packet loss, time %" PRIu32 "ms\n",
+           transmitted, received, loss, total_time_ms);
+
+    esp_ping_delete_session(hdl);
+}
+
+static void start_ping() {
+    ip_addr_t target_addr = {0};
+    struct addrinfo hint = {0}, *res = NULL;
+    esp_ping_config_t ping_cfg = ESP_PING_DEFAULT_CONFIG();
+    esp_ping_callbacks_t ping_cbs = {
+        .on_ping_success = ping_success_cb,
+        .on_ping_timeout = ping_timeout_cb,
+        .on_ping_end = ping_end_cb,
+        .cb_args = NULL
+    };
+
+    getaddrinfo(EXAMPLE_PING_SERVER, NULL, &hint, &res);
+    inet_addr_to_ip4addr(ip_2_ip4(&target_addr), &((struct sockaddr_in *)res->ai_addr)->sin_addr);
+    freeaddrinfo(res);
+
+    ping_cfg.count = 5;
+    ping_cfg.target_addr = target_addr;
+
+    esp_ping_new_session(&ping_cfg, &ping_cbs, &hd_ping);
+    esp_ping_start(hd_ping);
+}
+
+static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+    wifi_event_sta_connected_t* conn_event = NULL;
+    wifi_event_sta_disconnected_t* dis_event = NULL;
+    ip_event_got_ip_t* got_event = NULL;
+
+    if (WIFI_EVENT == event_base) {
+        switch (event_id) {
+        case WIFI_EVENT_STA_START:
+            ESP_LOGI(TAG, "wifi start connect, %s:%s", EXAMPLE_WIFI_SSID, EXAMPLE_WIFI_PWD);
+            esp_wifi_connect(); // non-block
+            break;
+        case WIFI_EVENT_STA_CONNECTED:
+            conn_event = (wifi_event_sta_connected_t *)event_data;
+            ESP_LOGI(TAG, "wifi connected, channel:%u authmode:%u", conn_event->channel, conn_event->authmode);
+            break;
+        case WIFI_EVENT_STA_DISCONNECTED:
+            dis_event = (wifi_event_sta_disconnected_t *)event_data;
+            ESP_LOGE(TAG, "wifi disconnected, reason:%u", dis_event->reason);
+            break;
+        default:
+            ESP_LOGW(TAG, "unknown WIFI_EVENT:%ld", event_id);
+            break;
+        }
+    }
+    
+    if (IP_EVENT == event_base) {
+        switch (event_id) {
+        case IP_EVENT_STA_GOT_IP:
+            got_event = (ip_event_got_ip_t *)event_data;
+            ESP_LOGI(TAG, "got ip, ip:" IPSTR " netmask:" IPSTR " gw:" IPSTR,
+                IP2STR(&got_event->ip_info.ip), IP2STR(&got_event->ip_info.netmask), IP2STR(&got_event->ip_info.gw));
+            start_ping();
+            break;
+        case IP_EVENT_STA_LOST_IP:
+            ESP_LOGE(TAG, "lost ip");
+            break;
+        default:
+            ESP_LOGW(TAG, "unknown IP_EVENT:%ld", event_id);
+            break;   
+        }
+    }
+}
+
+void app_main(void) {
+    esp_err_t err = ESP_OK;
+    wifi_init_config_t wifi_init_cfg = WIFI_INIT_CONFIG_DEFAULT();
+    wifi_config_t sta_cfg = {
+        .sta = {
+            .ssid = EXAMPLE_WIFI_SSID,
+            .password = EXAMPLE_WIFI_PWD,
+        },
+    };
+    uint32_t i = 0;
+
+    err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        nvs_flash_erase();
+        nvs_flash_init();
+    }
+
+    esp_event_loop_create_default();
+    esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, NULL);
+    esp_event_handler_instance_register(IP_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, NULL);
+
+    esp_netif_init();
+    esp_netif_create_default_wifi_sta();
+    esp_wifi_init(&wifi_init_cfg);
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_set_config(WIFI_IF_STA, &sta_cfg);
+    esp_wifi_start();
+
+    while (1) {
+        ESP_LOGI(TAG, "%lu", i++);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
